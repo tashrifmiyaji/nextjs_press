37d38a16154b3799ac1ceab9d6f801d5b325e66a
tashrif-miyaji <tashrifmiyaji@gmail.com>
2026-07-24 00:15:24 +0600

    chore: update dependencies and add new packages for improved functionality

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AuthGroupLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className='max-w-7xl mx-auto'> {children}</div>
+  )
+}
+
+export default AuthGroupLayout
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,21 @@
 import "./globals.css";
+import { Inter } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { Toaster } from "sonner";
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
+
+
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html
       lang="en"
-      className={`h-full antialiased`}
+      className={cn("h-full", "antialiased", "font-sans", inter.variable)}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+        <Toaster position="top-center" richColors/>
+      </body>
     </html>
   );
 }
